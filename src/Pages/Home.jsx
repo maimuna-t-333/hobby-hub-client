e0433e54b667a46f9bb43ea726223359a48844b9
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-05-21 03:37:04 +0600

    Challenges

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Components/Banner';
 import WhyJoin from '../Components/WhyJoin';
+import Challenges from '../Components/Challenges';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <WhyJoin></WhyJoin>
+            <Challenges></Challenges>
             
         </div>
     );
