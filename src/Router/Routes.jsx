3633118aaffbbed9e4204cac2f2a794d71f28982
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-05-20 22:51:09 +0600

    slider

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,17 +1,46 @@
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from "react-router";
 import MainLayout from "../Layout/MainLayout";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import CreateGroup from "../Pages/CreateGroup";
+import AllGroup from "../Pages/AllGroup";
+import MyGroup from "../Pages/MyGroup";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-  },
-  {
-    path:''
-  }
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                index: true,
+                element: <Home></Home>
+            },
+            {
+                path: 'allGroups',
+                element: <AllGroup></AllGroup>
+            }
+        ]
+    },
+    {
+        path: 'login',
+        element: <Login></Login>,
+        children: [{
+            path: 'register',
+            element: <Register></Register>
+        }]
+    },
+    {
+        path: 'createGroup',
+        element: <CreateGroup></CreateGroup>
+    },
+    {
+        path: 'myGroups',
+        element: <MyGroup></MyGroup>
+    }
 ]);
 
 export default router;
